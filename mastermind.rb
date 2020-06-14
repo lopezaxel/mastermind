866d17747afcbda8da302642dbabfebdde65f05a
Axel Lopez <lopezaxel@protonmail.com>
2020-06-14 11:54:48 -0300

    Updated feedbacks

--- mastermind.rb
+++ mastermind.rb
@@ -1,8 +1,35 @@
+# mastermind.rb
+
+require "pry"
+
 class Game
-  def initialize
+  def initialize(human, computer, board)
     @correct_guess = false
-    @max_number_guesses = 12
+    @max_guesses = 12
     @player_guesses = 0
+
+    @human = human
+    @computer = computer
+    @board = board
+  end
+
+  def start_game
+    until @correct_guess || @player_guesses > @max_guesses
+      player_guess = @human.make_guess
+      @correct_guess = check_guess(player_guess, @computer.code)
+      give_feedback(player_guess, @computer.code)
+
+      @player_guesses += 1
+    end
+
+    print "\nType 'yes' to restart the game: "
+    restart_answer = gets.chomp
+    # restart_game if restart_answer == "yes"
+  end
+
+  def restart_game
+    @computer.restart_computer
+    initialize
   end
 
   def check_guess(guess, code)
@@ -10,10 +37,13 @@ class Game
   end
 
   def give_feedback(guess, code)
+    copy_code = code.slice(0, code.length)
+
     guess.each_with_index do |letter, index|
-      if guess[index] == code[index]
+      if guess[index] == copy_code[index]
+        copy_code[index] = ""
         puts correct_letter_and_position(letter, index)
-      elsif code.include?(letter)
+      elsif copy_code.include?(letter)
         puts correct_letter(letter, index)
       end
     end
@@ -49,9 +79,15 @@ class Board
 end
 
 class Computer
+  attr_reader :code
+
   def initialize(board)
     @code = []
     @code = board.select_random_code(@code)
+  end
+
+  def restart_computer
+    initialize
   end
 end
 
@@ -61,15 +97,18 @@ class Human
   end
 
   def make_guess
-    print "Enter your guess: "
-    gets.chomp
+    print "\nEnter your guess: "
+    guess = gets.chomp
+    guess = guess.split("")
+
+    guess
   end
 end
 
 
-game = Game.new
 human = Human.new
 board = Board.new
 computer = Computer.new(board)
+game = Game.new(human, computer, board) 
 
-game.check_guess(["A", "B", "C", "D"], board.select_random_code([]))
+game.start_game
